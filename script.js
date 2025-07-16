8be866e8cb58c3317e8707fe6cfd0aa5e0a03efe
Nikhil Lanje <nikhillanje00@gmail.com>
2025-07-16 10:00:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function addToDisplay(value) {
+            document.getElementById('display').value += value;
+        }
+
+        // Function to clear the display
+        function clearDisplay() {
+            document.getElementById('display').value = '';
+        }
+
+        // Function to calculate the result
+        function calculate() {
+            try {
+                let result = eval(document.getElementById('display').value);
+                document.getElementById('display').value = result;
+            } catch (error) {
+                document.getElementById('display').value = 'Error';
+            }
+        }
